414f833823ce41decdd7750665246d7d526d2108
HVStudios <henke.vonbahr@gmail.com>
2020-04-08 01:51:52 +0200

    SLutprojekt progress

---
+++ slutprojekt/login/login.js
@@ -0,0 +1,43 @@
+/**@type {HTMLInputElement} */
+let usr;
+/**@type {HTMLInputElement} */
+let psw;
+/**@type {HTMLSpanElement} */
+let forgotPassword;
+/**@type {HTMLButtonElement} */
+let loginButton;
+
+window.onload = (e => {
+
+  usr = document.getElementById("usrname");
+  psw = document.getElementById("password");
+  loginButton = document.getElementById("loginbtn");
+
+  loginButton.onclick = (e => {
+
+    let email = usr.value;
+    let password = psw.value;
+
+    console.log(firebase.auth().currentUser);
+
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(async () => {
+      return firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
+        console.log("Yeet");
+        firebase.auth().onAuthStateChanged(user => {
+          if (user) {
+            location.href = "../index.html";
+          } else {
+            console.log(user);
+          }
+        });
+      }).catch(error => {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+      });
+    }).catch(function (error) {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+    })
+  });
+
+});
